e9fc26a1ea3de0dabd737209a094b403c8e487e2
Chen Dadon <chendaddon@gmail.com>
2020-06-15 22:07:31 +0300

    update git  tree

---
+++ Coffee/src/Test.cpp
@@ -0,0 +1,10 @@
+#include "Test.h"
+#include <stdio.h>
+
+namespace Coffee {
+
+	void Print()
+	{
+		printf("Welcome to Coffee Engine !\n");
+	}
+}
---
+++ Coffee/src/Test.h
@@ -0,0 +1,6 @@
+#pragma once
+
+namespace Coffee {
+
+	__declspec(dllexport) void Print();
+}
