f184c65b343c917f26a9af4714adecb9e6a4fc8e
Chen Dadon <chendaddon@gmail.com>
2020-08-05 12:23:02 +0300

    add log system

--- Coffee/src/Coffee/EndtryPoint.h
+++ Coffee/src/Coffee/EndtryPoint.h
@@ -2,16 +2,19 @@
 
 #ifdef CF_PLATFORM_WINDOWS
 
-#else
-	#error Coffee only support Windows!
-#endif // CF_PLATFORM_WINDOWS
-
 extern Coffee::Application* Coffee::CreateApplication();
 
 int main(int argc, char** argv)
 {
+	Coffee::Log::Init();
+	CF_CORE_WARN("Initialized Log!");
+	CF_INFO("Hello!");
+
 	auto app = Coffee::CreateApplication();
 	app->Run();
 
 	delete app;
 }
+
+#endif // CF_PLATFORM_WINDOWS
+
