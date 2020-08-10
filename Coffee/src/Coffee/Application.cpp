7588098f5782cb032a1f7d6354113ff1f5d163a2
Chen Dadon <chen.dadon@cu-bx.com>
2020-08-10 18:11:56 +0300

    Dispatching Events

--- Coffee/src/Coffee/Application.cpp
+++ Coffee/src/Coffee/Application.cpp
@@ -1,16 +1,18 @@
 #include "cfpch.h"
 #include "Application.h"
 
-#include "Coffee/Events/ApplicationEvent.h"
 #include "Coffee/Log.h"
 
 #include <GLFW/glfw3.h>
 
 namespace Coffee {
 
+#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
+
 	Application::Application()
 	{
 		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
 	}
 	
 	Application::~Application()
@@ -18,6 +20,14 @@ namespace Coffee {
 
 	}
 	
+	void Application::OnEvent(Event& e)
+	{
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+		
+		CF_CORE_TRACE("{0}", e);
+	}
+
 	void Application::Run()
 	{
 		while (m_Running)
@@ -26,5 +36,11 @@ namespace Coffee {
 			glClear(GL_COLOR_BUFFER_BIT);
 			m_Window->OnUpdate();
 		}
+	}
+
+	bool Application::OnWindowClose(WindowCloseEvent& e)
+	{
+		m_Running = false;
+		return true;
 	}
 }
--- Coffee/src/Coffee/Application.h
+++ Coffee/src/Coffee/Application.h
@@ -2,6 +2,8 @@
 
 #include "Core.h"
 #include "Events/Event.h"
+#include "Coffee/Events/ApplicationEvent.h"
+
 #include "Window.h"
 
 namespace Coffee {
@@ -14,7 +16,11 @@ namespace Coffee {
 	
 		void Run();
 
+		void OnEvent(Event& e);
+
 	private:
+		bool OnWindowClose(WindowCloseEvent& e);
+
 		std::unique_ptr<Window> m_Window;
 		bool m_Running = true;
 	};
