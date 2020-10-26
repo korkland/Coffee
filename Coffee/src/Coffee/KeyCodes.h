96f2467316adde49374e7820e7010b8134f43a5c
Chen Dadon <chendaddon@gmail.com>
2020-10-26 16:16:45 +0200

    add key and mosue codes

---
+++ Coffee/src/Coffee/KeyCodes.h
@@ -0,0 +1,125 @@
+#pragma once
+
+// From glfw3.h
+#define CF_KEY_SPACE              32
+#define CF_KEY_APOSTROPHE         39  /* ' */
+#define CF_KEY_COMMA              44  /* , */
+#define CF_KEY_MINUS              45  /* - */
+#define CF_KEY_PERIOD             46  /* . */
+#define CF_KEY_SLASH              47  /* / */
+#define CF_KEY_0                  48
+#define CF_KEY_1                  49
+#define CF_KEY_2                  50
+#define CF_KEY_3                  51
+#define CF_KEY_4                  52
+#define CF_KEY_5                  53
+#define CF_KEY_6                  54
+#define CF_KEY_7                  55
+#define CF_KEY_8                  56
+#define CF_KEY_9                  57
+#define CF_KEY_SEMICOLON          59  /* ; */
+#define CF_KEY_EQUAL              61  /* = */
+#define CF_KEY_A                  65
+#define CF_KEY_B                  66
+#define CF_KEY_C                  67
+#define CF_KEY_D                  68
+#define CF_KEY_E                  69
+#define CF_KEY_F                  70
+#define CF_KEY_G                  71
+#define CF_KEY_H                  72
+#define CF_KEY_I                  73
+#define CF_KEY_J                  74
+#define CF_KEY_K                  75
+#define CF_KEY_L                  76
+#define CF_KEY_M                  77
+#define CF_KEY_N                  78
+#define CF_KEY_O                  79
+#define CF_KEY_P                  80
+#define CF_KEY_Q                  81
+#define CF_KEY_R                  82
+#define CF_KEY_S                  83
+#define CF_KEY_T                  84
+#define CF_KEY_U                  85
+#define CF_KEY_V                  86
+#define CF_KEY_W                  87
+#define CF_KEY_X                  88
+#define CF_KEY_Y                  89
+#define CF_KEY_Z                  90
+#define CF_KEY_LEFT_BRACKET       91  /* [ */
+#define CF_KEY_BACKSLASH          92  /* \ */
+#define CF_KEY_RIGHT_BRACKET      93  /* ] */
+#define CF_KEY_GRAVE_ACCENT       96  /* ` */
+#define CF_KEY_WORLD_1            161 /* non-US #1 */
+#define CF_KEY_WORLD_2            162 /* non-US #2 */
+
+/* Function keys */
+#define CF_KEY_ESCAPE             256
+#define CF_KEY_ENTER              257
+#define CF_KEY_TAB                258
+#define CF_KEY_BACKSPACE          259
+#define CF_KEY_INSERT             260
+#define CF_KEY_DELETE             261
+#define CF_KEY_RIGHT              262
+#define CF_KEY_LEFT               263
+#define CF_KEY_DOWN               264
+#define CF_KEY_UP                 265
+#define CF_KEY_PAGE_UP            266
+#define CF_KEY_PAGE_DOWN          267
+#define CF_KEY_HOME               268
+#define CF_KEY_END                269
+#define CF_KEY_CAPS_LOCK          280
+#define CF_KEY_SCROLL_LOCK        281
+#define CF_KEY_NUM_LOCK           282
+#define CF_KEY_PRINT_SCREEN       283
+#define CF_KEY_PAUSE              284
+#define CF_KEY_F1                 290
+#define CF_KEY_F2                 291
+#define CF_KEY_F3                 292
+#define CF_KEY_F4                 293
+#define CF_KEY_F5                 294
+#define CF_KEY_F6                 295
+#define CF_KEY_F7                 296
+#define CF_KEY_F8                 297
+#define CF_KEY_F9                 298
+#define CF_KEY_F10                299
+#define CF_KEY_F11                300
+#define CF_KEY_F12                301
+#define CF_KEY_F13                302
+#define CF_KEY_F14                303
+#define CF_KEY_F15                304
+#define CF_KEY_F16                305
+#define CF_KEY_F17                306
+#define CF_KEY_F18                307
+#define CF_KEY_F19                308
+#define CF_KEY_F20                309
+#define CF_KEY_F21                310
+#define CF_KEY_F22                311
+#define CF_KEY_F23                312
+#define CF_KEY_F24                313
+#define CF_KEY_F25                314
+#define CF_KEY_KP_0               320
+#define CF_KEY_KP_1               321
+#define CF_KEY_KP_2               322
+#define CF_KEY_KP_3               323
+#define CF_KEY_KP_4               324
+#define CF_KEY_KP_5               325
+#define CF_KEY_KP_6               326
+#define CF_KEY_KP_7               327
+#define CF_KEY_KP_8               328
+#define CF_KEY_KP_9               329
+#define CF_KEY_KP_DECIMAL         330
+#define CF_KEY_KP_DIVIDE          331
+#define CF_KEY_KP_MULTIPLY        332
+#define CF_KEY_KP_SUBTRACT        333
+#define CF_KEY_KP_ADD             334
+#define CF_KEY_KP_ENTER           335
+#define CF_KEY_KP_EQUAL           336
+#define CF_KEY_LEFT_SHIFT         340
+#define CF_KEY_LEFT_CONTROL       341
+#define CF_KEY_LEFT_ALT           342
+#define CF_KEY_LEFT_SUPER         343
+#define CF_KEY_RIGHT_SHIFT        344
+#define CF_KEY_RIGHT_CONTROL      345
+#define CF_KEY_RIGHT_ALT          346
+#define CF_KEY_RIGHT_SUPER        347
+#define CF_KEY_MENU               348
